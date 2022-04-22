f14f00dae13cf4cda7876941daf6952626e2bbd5
Mike The Madkiwi <mike@hildyard.ca>
2022-04-22 07:44:52 -0400

    draw weather to screen

--- client.js
+++ client.js
@@ -134,7 +134,7 @@ let ClientGameTimer = setTick(async() => {
             draw2screen(`~o~DOW[~w~${dDay} ~o~] Date[ ~w~${DateTime["Date"][0]}/${DateTime["Date"][1]}/${DateTime["Date"][2]} ~o~]`, 255, 255, 255, 255, 0.02, 0.17, 0.4)
         }
         // let percwTrans = wTrans.toFixed(3)
-        let perc = (wTrans[2].toFixed(2) * 100)
+        let perc = wTrans[2].toFixed(2);
         draw2screen(`~o~Weather[ ~w~${wTrans[0]} < ${perc}% > ${wTrans[1]}~o~]`, 255, 255, 255, 255, 0.02, 0.19, 0.4)
         ///////////////////////////////////////
     }
